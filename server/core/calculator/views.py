c4051403b3c3258ea28ce5a08f9ff0d77c2a5113
Suryansh Kumar <suryansh@syn-172-101-202-178.res.spectrum.com>
2025-09-06 20:04:41 +0530

    calculatorapp

--- server/core/calculator/views.py
+++ server/core/calculator/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 
 def calc(request):
-    return render(request, 'calculator/calc.html') 
+    result = None
+    if request.method == 'POST':
+        num1 = float(request.POST['num1'])
+        num2 = float(request.POST['num2'])
+        op = request.POST['operation']
+        if op == 'add':
+            result = num1 + num2
+        elif op == 'subtract':
+            result = num1 - num2
+        elif op == 'multiply':
+            result = num1 * num2
+        elif op == 'divide':
+            result = num1 / num2 if num2 != 0 else 'Cannot divide by zero!'
+    return render(request, 'calculator/calc.html', {'res': result})
--- server/core/core/urls.py
+++ server/core/core/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
-from . import views
+
 
 urlpatterns = [
     path("", include("calculator.urls")),
