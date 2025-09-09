cade53953bad97b7e8933e7773224e59b1d842b9
Suryansh Kumar <suryansh@syn-172-101-202-178.res.spectrum.com>
2025-09-10 03:35:11 +0530

    QR share button, single QR logic, design and bug fixes

--- server/core/qr/views.py
+++ server/core/qr/views.py
@@ -1,20 +1,29 @@
-
 from django.shortcuts import render
 import qrcode
 from io import BytesIO
 import base64
 
 def qr(request):
-    qr_image = None
-    text = ''
+    qr_image = None   # latest QR only
+    text = ''         # input field value
+
     if request.method == 'POST':
-        text = request.POST.get('text', '')
-        if text:
-            qr = qrcode.QRCode(version=1, box_size=10, border=5)
-            qr.add_data(text)
-            qr.make(fit=True)
-            img = qr.make_image(fill='black', back_color='white')
-            buffer = BytesIO()
-            img.save(buffer, format='PNG')
-            qr_image = base64.b64encode(buffer.getvalue()).decode('utf-8')
-    return render(request, 'qr/qr.html', {'qr_image': qr_image, 'text': text})
+        # Clear All QR Codes logic (ab waise bhi ek hi QR hai, fir bhi future proof)
+        if 'clear' in request.POST:
+            qr_image = None
+            text = ''
+        else:
+            text = request.POST.get('text', '')
+            if text:
+                qr_code = qrcode.QRCode(version=1, box_size=10, border=5)
+                qr_code.add_data(text)
+                qr_code.make(fit=True)
+                img = qr_code.make_image(fill='black', back_color='white')
+                buffer = BytesIO()
+                img.save(buffer, format='PNG')
+                qr_image = base64.b64encode(buffer.getvalue()).decode('utf-8')
+
+    return render(request, 'qr/qr.html', {
+        'qr_image': qr_image,
+        'text': '',
+    })
